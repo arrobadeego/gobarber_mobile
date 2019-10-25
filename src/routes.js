7c0b9e3c28c44d623395dd5aee90ae03d8402cca
Diego Moreira <diegomptcc@hotmail.com>
2019-10-25 13:18:11 -0300

    routes config

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,13 @@
-import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-const Routes = createAppContainer();
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+const Routes = createAppContainer(
+    createSwitchNavigator({
+        SignIn,
+        SignUp,
+    })
+);
 
 export default Routes;
