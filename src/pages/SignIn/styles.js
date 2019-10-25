7ddafcfda61cdba5ec5bf5bd491fb989f75cd802
Diego Moreira <diegomptcc@hotmail.com>
2019-10-25 19:43:49 -0300

    finish style on sign in page

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -4,7 +4,10 @@ import styled from 'styled-components/native';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-export const Container = styled.View`
+export const Container = styled.KeyboardAvoidingView.attrs({
+    enabled: Platform.OS === 'ios',
+    behavior: 'padding',
+})`
     flex: 1;
     justify-content: center;
     align-items: center;
