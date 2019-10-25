5e6d5ff74c6fdd29a97edcd9a0cc546dcdfff2ed
Diego Moreira <diegomptcc@hotmail.com>
2019-10-25 16:15:29 -0300

    create Input

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '../../components/Input';
 import Background from '../../components/Background';
 // import { Container } from './styles';
 
@@ -8,6 +9,12 @@ export default function SignIn() {
     return (
         <Background>
             <Text>Sign In</Text>
+
+            <Input
+                style={{ marginTop: 30 }}
+                icon="call"
+                placeholder="Digite seu nome"
+            />
         </Background>
     );
 }
