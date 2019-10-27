c4660f0daa052a462b91d80c96feffb2b376573c
Diego Moreira <diegomptcc@hotmail.com>
2019-10-27 11:54:36 -0300

    style tabbar

--- src/routes.js
+++ src/routes.js
@@ -10,18 +10,30 @@ import Dashboard from './pages/Dashboard';
 
 export default (isSigned = false) =>
     createAppContainer(
-        createSwitchNavigator({
-            Sign: createSwitchNavigator({
-                SignIn,
-                SignUp,
-            }),
-            App: createBottomTabNavigator(
-                {
-                    Dashboard,
-                },
-                {
-                    initialRouteName: isSigned ? 'App' : 'Sign',
-                }
-            ),
-        })
+        createSwitchNavigator(
+            {
+                Sign: createSwitchNavigator({
+                    SignIn,
+                    SignUp,
+                }),
+                App: createBottomTabNavigator(
+                    {
+                        Dashboard,
+                    },
+                    {
+                        tabBarOptions: {
+                            keyboardHidesTabBar: true,
+                            activeTintColor: '#fff',
+                            inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+                            style: {
+                                backgroundColor: '#8d41a8',
+                            },
+                        },
+                    }
+                ),
+            },
+            {
+                initialRouteName: isSigned ? 'App' : 'Sign',
+            }
+        )
     );
