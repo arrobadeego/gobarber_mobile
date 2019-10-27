e8e64a73d2e02936e1f33fb9b2b49c2e9888af40
Diego Moreira <diegomptcc@hotmail.com>
2019-10-27 19:44:11 -0300

    finish appointments screen

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '../../components/Background';
+
+import api from '../../services/api';
 
 import { Container, Title, List } from './styles';
 import Appointment from '../../components/Appointment';
 
-const data = [1, 2, 3, 4, 5];
-
 export default function Dashboard() {
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+        async function loadAppointments() {
+            const response = await api.get('/appointments');
+
+            setAppointments(response.data);
+        }
+
+        loadAppointments();
+    }, []);
+
+    async function handleCancel(id) {
+        const response = await api.delete(`appointments/${id}`);
+
+        setAppointments(
+            appointments.map(appointment =>
+                Appointment.id === id
+                    ? {
+                          ...appointment,
+                          canceled_at: response.data.canceled_at,
+                      }
+                    : appointment
+            )
+        );
+    }
+
     return (
         <Background>
             <Container>
                 <Title>Agendamentos</Title>
 
                 <List
-                    data={data}
-                    keyExtrator={item => String(item)}
-                    renderItem={({ item }) => <Appointment data={item} />}
+                    data={appointments}
+                    keyExtrator={item => String(item.id)}
+                    renderItem={({ item }) => (
+                        <Appointment
+                            onCancel={() => handleCancel(item.id)}
+                            data={item}
+                        />
+                    )}
                 />
             </Container>
         </Background>
